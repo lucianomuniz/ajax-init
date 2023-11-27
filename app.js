96b60db8dfd95b0c2e271b1d12f78bd776bd23f9
lucianomuniz <luciano.mo@gmail.com>
2023-11-27 11:34:28 -0300

    refactor: use async/await statements

--- app.js
+++ app.js
@@ -65,54 +65,54 @@ const checkStatus = (response) => {
   }
 }
 
-const showImage = () => {
-// ** FETCHING THE IP ADDRESS
-let url = 'http://worldtimeapi.org/api/timezone/Africa/Harare'
-// use the Headers Interface
-let h = new Headers()
-h.append('Accept', 'application/json')
-// use the Request Interface
-let req = new Request(url, {
-  method: 'GET',
-  headers: h
-})
+const showImage = async () => {
+  // ** FETCHING THE IP ADDRESS
+  let url = 'http://worldtimeapi.org/api/timezone/Africa/Harare'
+  // use the Headers Interface
+  let h = new Headers()
+  h.append('Accept', 'application/json')
+  // use the Request Interface
+  let req = new Request(url, {
+    method: 'GET',
+    headers: h
+  })
 
 // define our AJAX fetch request
-fetch(req)
-  .then(checkStatus)
-  .then(res => res.json())
-  .then( data => document.getElementById('iptext').innerHTML = data.client_ip)
-  .catch(e =>console.log('Oops, there was a network error:', e.message))
+  try{
+    const res = await fetch(req).then(res => checkStatus(res))
+    const data = await res.json(res)
+    document.getElementById('iptext').innerHTML = data.client_ip
+  } catch(e) {
+    console.log('Oops, there was a network error:', e.message)
+  } 
 
   // ** FETCHING THE IMAGE
   let showBtn = document.getElementById('btn-img')
-  let imageURL = "./ajax.png"
+  let imageURL = './ajax.png'
   let imageElement = document.getElementById('ajax-img')
   // add an event listener
   showBtn.addEventListener('click', fetchImage)
 
   // create our fetchImage function
-  function fetchImage() {
-  // using the Headers Interface
-  let h = new Headers()
-  h.append('Accept', 'image/png')
-  // using the Request Interface
-  const req = new Request(imageURL, {
-      method: 'GET',
-      headers: h
-  }); 
-  
-  // perform our fetch call
-  fetch(req)
-    .then(checkStatus)
-    .then(res => res.blob())
-    .then(imgObj => {
-        const pictureURL = URL.createObjectURL(imgObj)
-        imageElement.src = pictureURL
-    })
-    .catch(e => {
-        console.log('Something went wrong:', e.message)
-    })
+  async function fetchImage() {
+    // using the Headers Interface
+    let h = new Headers()
+    h.append('Accept', 'image/png')
+    // using the Request Interface
+    const req = new Request(imageURL, {
+        method: 'GET',
+        headers: h
+    }); 
+    
+    // perform our fetch call
+    try{
+      const res = await fetch(req).then(res => checkStatus(res))
+      const imgObj = await res.blob()
+      const pictureURL = URL.createObjectURL(imgObj)
+      imageElement.src = pictureURL
+    } catch(e) {
+      console.log('Something went wrong:', e.message)
+    }
   }
 }
 
